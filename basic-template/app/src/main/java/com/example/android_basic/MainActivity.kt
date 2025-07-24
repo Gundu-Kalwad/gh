52f27bacd7fd946db7376a9850826c2701980544
Gundu-Kalwad <gundu6362@gmail.com>
2025-07-24 18:23:42 +0530

    Upload project files

--- basic-template/app/src/main/java/com/example/android_basic/MainActivity.kt
+++ basic-template/app/src/main/java/com/example/android_basic/MainActivity.kt
@@ -1,9 +1,9 @@
 // MainActivity.kt
 // This is the main entry point for your Android app's UI.
 // It displays the "Hello World!" screen and handles window insets for modern edge-to-edge layouts.
-//jdj
+
 package com.yourcompany.yourapp
-////65577
+
 // Import essential Android and Jetpack libraries
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge // For drawing behind system bars
